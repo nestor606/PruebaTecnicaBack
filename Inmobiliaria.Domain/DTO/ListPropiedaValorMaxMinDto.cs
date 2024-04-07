b9c68e24ac9810fb114a79a2d4cccd9c8c7957ed
Ivan MaquiloN Meza <Maquilon.927@gmail.com>
2024-04-06 23:29:58 -0500

    Ajustes de usecase

---
+++ Inmobiliaria.Domain/DTO/ListPropiedaValorMaxMinDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria.Domain.DTO
+{
+    public class ListPropiedaValorMaxMinDto
+    {
+        public int Maximo { get; set; }
+        public int Minimo { get; set; }
+    }
+}
---
+++ Inmobiliaria.Domain/UseCase/Interfaces/Ipropiedades.cs
@@ -0,0 +1,18 @@
+﻿using Inmobiliaria.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria.Domain.UseCase.Interfaces
+{
+    public interface Ipropiedades
+    {
+        List<ListPropiedaValorMaxMinDto> GetValorMaxMinPropiedades();
+        Task<PropiedadDto> AgregarPropiedad(PropiedadDto propiedad);
+
+
+    }
+}
+ 
